c250c23cd8f4423f3220bfc6a1f64b5499be3600
Eremey Lazar <elazar1926@gmail.com>
2023-12-14 15:02:27 +0300

    -auth

--- src/main/java/ru/kata/spring/boot_security/demo/securityConfig/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/securityConfig/SecurityConfig.java
@@ -1,22 +1,31 @@
 package ru.kata.spring.boot_security.demo.securityConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kata.spring.boot_security.demo.service.UsersDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final UsersDetailsService usersDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UsersDetailsService usersDetailsService) {
+        this.usersDetailsService = usersDetailsService;
     }
 
     //нфстраивает аутентификацию
-    protected void configure (AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersDetailsService);
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder () {
+        return NoOpPasswordEncoder.getInstance();
     }
 }

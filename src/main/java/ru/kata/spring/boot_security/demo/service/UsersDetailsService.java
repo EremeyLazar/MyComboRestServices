e75e3aebd0846e96fac2dd13d4da81e9218a31b3
Eremey Lazar <elazar1926@gmail.com>
2023-12-14 14:45:33 +0300

    after merge

--- src/main/java/ru/kata/spring/boot_security/demo/service/UsersDetailsService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UsersDetailsService.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.kata.spring.boot_security.demo.security.UsersDetails;
 
 import java.util.Optional;

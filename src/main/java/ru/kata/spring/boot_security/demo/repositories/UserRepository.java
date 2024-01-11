1c52b5bf624aaa8673f11a7d731380e2e174bf89
Eremey Lazar <elazar1926@gmail.com>
2024-01-11 13:42:06 +0300

    DatabaseInitializer

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
     User findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
 }
